4a4733489d48979147458a3138dda0651960722e
Adam Wittmann <adamjwittmann@gmail.com>
2026-05-12 02:09:13 -0400

    demo vid

--- compiler-project/JavaCode/AlanProgram1.java
+++ compiler-project/JavaCode/AlanProgram1.java
@@ -1,17 +1,7 @@
 public class AlanProgram1 {
     public static void main(String[] args) {
-        int x;
-        String s;
-        boolean b;
-        x = 0;
-        s = "hello world";
-        b = true;
-        while ((x != 5)) {
-            System.out.println(x);
-            x = 1 + x;
-        }
-        if ((b == true)) {
-            System.out.println(s);
-        }
+        int a;
+        a = 7;
+        System.out.println(a);
     }
 }
---
+++ compiler-project/JavaCode/AlanProgram2.java
@@ -0,0 +1,7 @@
+public class AlanProgram2 {
+    public static void main(String[] args) {
+        int b;
+        b = 2;
+        System.out.println(b);
+    }
+}
